cca47f2f0df3716428fdd047c090bc35d517cb95
Iurchenko Nikita <73344097+be3dna@users.noreply.github.com>
2025-07-06 13:12:52 +0300

    Add import of environment variables from .env file
    #11

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
+
 from telegram.ext import ApplicationBuilder
 
+from app.config import TELEGRAM_TOKEN
 from bot.handlers import conversation_handler
+
 
 def main() -> None:
     application = (
         ApplicationBuilder()
-        .token("ТОКЕН ХИАР")
+        .token(TELEGRAM_TOKEN)
         .build()
     )
     application.add_handler(conversation_handler)
